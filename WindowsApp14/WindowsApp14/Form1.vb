6c40df697f83f6224611ae20db3558578cb3cf1d
Mr.House <109467924+MrHouse2020@users.noreply.github.com>
2023-04-08 14:47:44 +0530

    Add files via upload

---
+++ WindowsApp14/WindowsApp14/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2, div As Integer
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+        Try
+            div = num1 / num2
+            MsgBox("divison" & div)
+        Catch ex As OverflowException
+            MsgBox("divison of {0} by zero", num2)
+
+
+        End Try
+    End Sub
+End Class
